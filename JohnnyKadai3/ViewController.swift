cc0891b7a455f936c653858e7c07b386d72a9f3e
0102johnny <rt2824.recruit.1022@gmail.com>
2022-03-20 16:41:10 +0900

    同じ処理を繰り返していた箇所等を修正

--- JohnnyKadai3/ViewController.swift
+++ JohnnyKadai3/ViewController.swift
@@ -8,15 +8,11 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    @IBOutlet private weak var firstValueTextField: UITextField!
+    @IBOutlet private var valueTextFields: [UITextField]!
 
-    @IBOutlet private weak var secondValueTextField: UITextField!
+    @IBOutlet private var valueSwitches: [UISwitch]!
 
-    @IBOutlet private weak var firstValueSwitch: UISwitch!
-
-    @IBOutlet private weak var secondValueSwitch: UISwitch!
-
-    @IBOutlet private weak var firstValueLabel: UILabel!
+    @IBOutlet weak private var firstValueLabel: UILabel!
 
     @IBOutlet private weak var secondValueLabel: UILabel!
 
@@ -24,18 +20,16 @@ final class ViewController: UIViewController {
 
     @IBAction private func executeCalculation(_ sender: Any) {
         do {
-            // 三項演算子で処理をコンパクトに実装
-            let firstValue = firstValueTextField.textToInt * (firstValueSwitch.isOn ? -1 : 1)
-            let secondValue = secondValueTextField.textToInt * (secondValueSwitch.isOn ? -1 : 1)
+            // textFieldの入力値をInt型に変換する
+            let values = valueTextFields.map { $0.textToInt }
+            // zipメソッドで配列をペアにしたタプルを作る。mapメソッドで各配列の要素にアクセスし三項演算子で符号の切り替えを実装
+            let signNums = zip(valueSwitches, values).map { $0.isOn ? -$1 : $1 }
+            // reduceメソッドで合計値を算出し、OverflowErrorハンドリングも実装。
+            let totalValues = try values.reduce(0) { try $0.addingReportingOverflowWithError($1) }
 
-            // reduceを使って冗長なコードをコンパクトに実装
-            let totalValue = try [firstValue, secondValue].reduce(0, {
-                try $0.addingReportingOverflowWithError($1)
-            })
-
-            firstValueLabel.text = String(firstValue)
-            secondValueLabel.text = String(secondValue)
-            resultLabel.text = String(totalValue)
+            firstValueLabel.text = String(signNums[0])
+            secondValueLabel.text = String(signNums[1])
+            resultLabel.text = String(totalValues)
         } catch {
             switch error {
             case is OverflowError:
